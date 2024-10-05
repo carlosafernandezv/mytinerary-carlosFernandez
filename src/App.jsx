abb376848b0e1949627b98be09a165335101fc27
Carlos Fernandez <krlosafernandez@gmail.com>
2024-10-05 10:55:52 -0500

    cambio de pc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 function App() {
   return (
+    <>
     <div className="h-screen bg-indigo-500 grid place-items-center">App</div>
+    <Carousel></Carousel>
+  
+  </>
   )
 }
 export default App
