19ef45a6361532a43597b2ba37b3f2607ed8c6be
Carlos Fernandez <krlosafernandez@gmail.com>
2024-10-05 12:04:05 -0500

    cree el navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Carousel from "./components/Carousel"
+import Navbar from './components/Navbar'
 function App() {
   return (
     <>
+    <Navbar/>
     <div className="h-screen bg-indigo-500 grid place-items-center">App</div>
     <Carousel></Carousel>
-  
+    
   </>
   )
 }
