fe4fcff020d7c45a8a58fd534e6cffc6ae11f606
Carlos Fernandez <krlosafernandez@gmail.com>
2024-10-12 12:14:34 -0500

    complete la rutas cities y standard layout agregue un diseño a error 404

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,36 @@
 import './App.css'
-import Hero from './components/Hero'
-import Header from './components/Header'
-import { Slider } from './Slider'
-import slides from './mock.json'
-import Footer from './components/Footer';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Cities from './Pages/Cities'
 import NotFound from './Pages/NotFound'
 import Home from './Pages/Home'
+import StandardLayout from './Layouts/StandardLayout'
 
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home></Home> },
-  { path: "/Home", element: <Home></Home> },
-  { path: "/Cities", element: <Cities></Cities> },
+  {
+    element: <StandardLayout></StandardLayout>,
+    children:
+      [
+        { path: "/", element: <Home></Home> },
+        { path: "/Home", element: <Home></Home> },
+        { path: "/Cities", element: <Cities></Cities> },
+      ]
+  },
   { path: "/*", element: <NotFound></NotFound> },
 ])
 function App() {
 
   return (
     <>
-    <RouterProvider router={router}></RouterProvider>
-      <Header />
-      <Hero />
-      <Slider slides={slides} />
+      <RouterProvider router={router}></RouterProvider>
+      
+      
 
 
 
-      <Footer />
+
+
+      
 
 
 
--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
-
+import Header from '../components/Header'
+import bgNotFound from "../assets/bgNotFound.jpg"
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+      <Header> </Header>
+      <div className="w-full relative overflow-hidden " style={{ height: 'calc(100vh - 4rem)' }}>
+        <img
+          src={bgNotFound}
+          alt="Explore"
+          className="w-full h-full object-cover absolute object-center brightness-[.30] shadow-lg animate-zoom"
+        />
+
+        <div className="relative isolate px-6 pt-14 lg:px-8 ">
+
+        <div className="text-center">
+          <p className="text-8xl font-bold text-white ">4 0 4</p>
+        <p className="text-4xl font-bold mb-1 text-white" >YES, YOU ARE LOST</p>
+        <p className="text-2xl font-bold text-white">That road didnt took you</p>
+        <p className="text-2xl font-bold text-white">anywhere.</p>
+            
+          </div>
+        </div>
+
+
+
+      </div>
+
+    </>
   )
 }
