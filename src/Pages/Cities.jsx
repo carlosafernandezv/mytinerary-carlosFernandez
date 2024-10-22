7bac8f611630ff648efa62fa32930c0d99e7fc19
Carlos Fernandez <krlosafernandez@gmail.com>
2024-10-22 06:22:00 -0500

    cree los componentes cards y search

--- src/Pages/Cities.jsx
+++ src/Pages/Cities.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Search from '../components/Search'
+import Cards from '../components/Cards'
 
 export default function Cities() {
   return (
     <div className="container w-full min-h-[80vh] mx-auto py-10">      
       <h1 className="text-4xl font-bold mb-4">Cities</h1>
-      <p>esto es city.</p>
+      <Search />
+      <Cards />
+      <Cards />
+      <Cards />                                                                                                                                                                                                                                                                                                                             
     </div>
   )
 }
